f08a570c7675d338d794ff70935c933d346d11d2
qualitytrash1 <eggzlolo@gmail.com>
2026-01-11 16:48:52 -0700

    s

--- scripts/newspaper.gd
+++ scripts/newspaper.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 
 func _ready() -> void:
 	hide()
-
+#
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("delete") or Input.is_action_just_pressed("settings"):
 		if visible:
