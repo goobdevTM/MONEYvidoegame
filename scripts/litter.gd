62bf25c4da31ca7be45a0d5fe4c0ed6027c10999
goobdevTM <getfreegame631@gmail.com>
2026-01-10 22:36:02 -0700

    csd

--- scripts/litter.gd
+++ scripts/litter.gd
@@ -55,9 +55,9 @@ func _on_delete_check_area_entered(area: Area2D) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	sprite.show()
-	area_2d.disabled = false
+	area_2d.get_child(0).disabled = false
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	sprite.hide()
-	area_2d.disabled = true
+	area_2d.get_child(0).disabled = true
