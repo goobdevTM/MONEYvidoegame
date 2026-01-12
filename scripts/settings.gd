36703f9dc7501b204ec57214968c232063732158
goobdevTM <getfreegame631@gmail.com>
2026-01-11 17:07:18 -0700

    wfrga

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -36,7 +36,7 @@ func _ready() -> void:
 	hide()
 
 func _process(delta: float) -> void:
-	not Globals.in_smellizon and not Globals.in_storage and not Globals.in_help
+	if not Globals.in_smellizon and not Globals.in_storage and not Globals.in_help:
 		if Input.is_action_just_pressed("settings"):
 			Globals.in_settings = not Globals.in_settings
 			if Globals.in_settings:
